ba323a33b13c375ec964cc574391fb299b35a31d
Arfin Dwi Octavianto <arfindwioctavianto@gmail.com>
2024-10-13 03:05:22 +0700

    feat: add search for active and archived notes

--- src/components/NoteAppBody.jsx
+++ src/components/NoteAppBody.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import NoteInput from "./NoteInput";
 import NotesList from "./NotesList";
 
-function NoteAppBody({ notes, addNote, onDelete, onArchive, onActive }) {
-  const activeNotes = notes.filter((note) => note.archived === false);
-  const archiveNotes = notes.filter((note) => note.archived === true);
+function NoteAppBody({ notes, addNote, onDelete, onArchive, onActive, onSearch }) {
+  const activeNotes = notes.filter((note) => note.archived === false).filter((note) => note.title.toLowerCase().includes(onSearch.toLowerCase()));
+  const archiveNotes = notes.filter((note) => note.archived === true).filter((note) => note.title.toLowerCase().includes(onSearch.toLowerCase()));
   return (
     <div className="note-app__body">
       <div className="note-input">
